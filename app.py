3bb58e56cdb58a54538132c6784cbd10320613c3
Kyle R Wenholz <kyle@krwenholz.com>
2015-12-07 22:32:26 -0800

    More meaningful first page

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask
+import time
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return "Hello World! I am Kyle's at time " + time.clock()
 
 if __name__ == "__main__":
         app.run()
