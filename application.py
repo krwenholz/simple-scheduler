b756e4a2c91786b82894dcb198f93bbe9e23d24e
Kyle R Wenholz <kyle@krwenholz.com>
2015-12-10 17:39:47 -0800

    Dynamo writes work and save user data
    Reads still don't work. . . .

--- application.py
+++ application.py
@@ -1,9 +1,27 @@
 from flask import Flask, render_template
+from simple_scheduler.dynamo import ConnectionManager, UserStore
+
 app = Flask(__name__)
+connection_manager = ConnectionManager()
+user_store = UserStore(connection_manager)
 
 @app.route('/')
 def hello():
     return render_template('index.html')
 
+@app.route('/client/<client_name>', methods=['POST'])
+def create_client(client_name):
+    user_store.create_user(client_name, 'client')
+    return 'Client "{}" created'.format(client_name) 
+
+@app.route('/specialist/<specialist_name>', methods=['POST'])
+def create_specialist(specialist_name):
+    user_store.create_user(specialist_name, 'specialist')
+    return 'Specialist "{}" created'.format(specialist_name) 
+
+@app.route('/user/<username>', methods=['GET'])
+def get_user(username):
+    return user_store.get_user(username)
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
---
+++ simple_scheduler/dynamo.py
@@ -0,0 +1,52 @@
+import boto3, os 
+from datetime import datetime
+
+class ConnectionManager:
+    """
+    An abstraction for establishing connections to DynamoDB and managing our
+    basic tables: users and calendars
+    XXX: Reads AWS secrets from environment variables right now, this should 
+        be improved: allow config file or command line args
+    """
+    def __init__(self):
+        """
+        Return our basic DynamoDB connection
+        TODO: connect to a local test instance of DynamoDB when appropriate
+        """
+        self.db = boto3.resource('dynamodb')
+
+    def table(self, name):
+        try:
+            return self.db.Table(name)
+        except Exception as e:
+            raise Exception("Failed to connect to users table", e)
+
+class UserStore:
+    """
+    Manages user information in DynamoDB
+    TODO: I should create a namedtuple class to represent users
+    """
+    def __init__(self, connection_manager):
+        self.connection_manager = connection_manager
+        self.users_table = self.connection_manager.table('simple-scheduler-users')
+
+    def get_user(self, username):
+        try:
+            return self.users_table.get_item(Key={'username':username})
+        except Exception as e:
+            #TODO: update this error handling to be more polite with Boto3
+            print('User [{}] was not found!'.format(username))
+            return None
+
+    def create_user(self, username, user_type):
+        create_date = str(datetime.now())
+        self.users_table.put_item(
+                Item={
+                    "username" : username,
+                    "type" : user_type,
+                    "create_date" : create_date
+                    })
+
+
+
+
