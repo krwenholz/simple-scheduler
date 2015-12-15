0d2a169d8d4adcd9b698ae14dcf8a4eb9323377c
Kyle R Wenholz <kyle@krwenholz.com>
2015-12-14 22:36:56 -0800

    Update integration test to be more interesting
    To be fair, it's not really a test since we make no assertions,
    but it provides an easy script with which to test user interactions

--- bin/simple_integration.py
+++ bin/simple_integration.py
@@ -3,7 +3,7 @@
 # client 'client_0', client 'client_1', specialist 'specialist_0', and specialist 'specialist_1'
 # TODO: create the clients at start, if needed, and clean up afterwards
 from urllib import request
-import datetime, calendar
+import datetime, calendar, time
 
 def put_event_request(requester, username, time):
     return request.urlopen(request.Request('http://localhost:5000/events/{}/{}/{}'.format(
@@ -25,8 +25,6 @@ month_start = calendar.timegm(datetime.datetime.today().replace(day=1).timetuple
 print('Putting free time on schedules')
 print(put_event_request('specialist_0', 'specialist_0', month_start).read())
 print(put_event_request('specialist_0', 'specialist_0', month_start + 600).read())
-print(put_event_request('specialist_1', 'specialist_1', month_start + 1200).read())
-print(put_event_request('specialist_1', 'specialist_1', month_start + 2400).read())
 
 print('Looking at free events for this month')
 print(current_month_request('specialist_0', 'specialist_0').read())
@@ -34,6 +32,8 @@ print(current_month_request('client_0', 'specialist_0').read())
 
 print('Booking a specialist_0 slot for client_0')
 print(put_event_request('client_0', 'specialist_0', month_start).read())
+print('Sleeping a few seconds because no human is this fast!')
+time.sleep(6)
 print(current_month_request('client_0', 'client_0').read())
 print(current_month_request('client_0', 'specialist_0').read())
 
@@ -43,6 +43,4 @@ print(current_month_request('client_1', 'specialist_0').read())
 print('Cleaning out the schedules')
 print(delete_event_request('specialist_0', 'specialist_0', month_start).read())
 print(delete_event_request('specialist_0', 'specialist_0', month_start + 600).read())
-print(delete_event_request('specialist_1', 'specialist_1', month_start + 1200).read())
-print(delete_event_request('specialist_1', 'specialist_1', month_start + 2400).read())
 
