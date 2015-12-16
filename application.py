3f6aae69f5a90fa546176b1f64eea792d1e103f4
Kyle R Wenholz <kyle@krwenholz.com>
2015-12-15 21:37:27 -0800

    Turn off debug (it isn't safe)

--- application.py
+++ application.py
@@ -1,13 +1,15 @@
 from flask import Flask, render_template
 from simple_scheduler.rest_views import UsersView, EventsView
+from simple_scheduler.visual_interaction import VisualView
 
 app = Flask(__name__, template_folder='simple_scheduler/templates')
 UsersView.register(app)
 EventsView.register(app)
+VisualView.register(app)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
