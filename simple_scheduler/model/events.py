f3d1be180968b5a80b0c38bda957259e84376119
Kyle R Wenholz <kyle@krwenholz.com>
2015-12-15 23:32:37 -0800

    Remove unused class file

--- simple_scheduler/model/events.py
+++
@@ -1,9 +0,0 @@
-from collections import namedtuple
-
-class Event(namedtuple('Event', 'username datetime type create_date')):
-    def from_ddb(ddb_response):
-        event_dict = ddb_response['Item']
-        return User(user_dict['username'], 
-                user_dict['type'], 
-                user_dict['email'], 
-                user_dict['create_date'])
