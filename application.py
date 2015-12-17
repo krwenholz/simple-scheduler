c0bf60a56b319671cdd2e02f573370ee619f145f
Kyle R Wenholz <kyle@krwenholz.com>
2015-12-16 22:04:59 -0800

    Write to allowed location and remove bad imports

--- application.py
+++ application.py
@@ -1,10 +1,9 @@
 from flask import Flask, render_template
 from simple_scheduler.rest_views import UsersView, EventsView
 from simple_scheduler.visual_interaction import VisualView
-from simple_scheduler import setup_aws
 import logging
 
-logging.basicConfig(filename='/var/log/simple_scheduler.log', level=logging.INFO)
+logging.basicConfig(filename='/tmp/simple_scheduler.log', level=logging.INFO)
 
 template_path = 'simple_scheduler/templates'
 
